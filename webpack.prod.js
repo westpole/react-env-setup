97a62ab9555627e8ed2ff0a0453fb0e5e136d3f3
Stanislav Spuzyak <stanislav.spuzyak@gmail.com>
2018-10-31 09:17:19 -0400

    Build: disable source map for production (task #18)

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: false,
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
