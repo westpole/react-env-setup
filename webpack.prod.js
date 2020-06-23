4a62c7d8cbcbaa3caba0a6648097c7e5561199d0
Stanislav Spuzyak <stanislav.spuzyak@gmail.com>
2020-06-22 22:36:19 -0400

    build: lean dependencies list and remove CSS/SASS preprocessors from the build list

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const common = require('./webpack.common.js');
 
@@ -16,46 +15,6 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
     new BundleAnalyzerPlugin()
   ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: false,
-              importLoaders: 1
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              sourceMap: true,
-              plugins: (loader) => [
-                require('postcss-import')({ root: loader.resourcePath }),
-                require('precss')(),
-                require('autoprefixer')(),
-                require('cssnano')({ autoprefixer: false })
-              ]
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: false
-            }
-          }
-        ]
-      }
-    ]
-  }
 });
